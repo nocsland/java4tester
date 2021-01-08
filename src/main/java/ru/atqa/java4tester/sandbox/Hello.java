84fd2c79a801bd4737e60ceb1e4dd098e8a2a0a9
Anton Vlasov <nocsland@gmail.com>
2021-01-08 22:54:25 +0400

    Добавлены функции
    Добавлена функция печати  (hello)
    Добавлена функция для расчета площади квадрата
    Добавлена функция для расчета площади прямоугольника

--- src/main/java/ru/atqa/java4tester/sandbox/Hello.java
+++ src/main/java/ru/atqa/java4tester/sandbox/Hello.java
@@ -1,9 +1,28 @@
 package ru.atqa.java4tester.sandbox;
 
 public class Hello {
-  public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    public static void main(String[] args) {
+        hello("world!");
+        hello("user!");
+        hello("Anton!");
+        double l = 5;
+        double a = 3;
+        double b = 6;
+      System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area (a, b));
 
-  }
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody);
+    }
+    public static double area (double len){
+      return len * len;
+    }
+    public static double area (double a, double b){
+      return a * b;
+
+    }
+
 
 }

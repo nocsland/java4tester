87b0abbd5dc7bbfdd1a989773d8a9f6076153eda
Anton Vlasov <nocsland@gmail.com>
2021-01-09 18:15:47 +0400

    функции заменены на классы и методы

--- src/main/java/ru/atqa/java4tester/sandbox/Hello.java
+++ src/main/java/ru/atqa/java4tester/sandbox/Hello.java
@@ -5,23 +5,20 @@ public class Hello {
         hello("world!");
         hello("user!");
         hello("Anton!");
-        double l = 5;
-        double a = 3;
-        double b = 6;
-      System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
-      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area (a, b));
+
+        Square s = new Square(5);
+
+        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
+
+        Rectangle r = new Rectangle(3, 6);
+
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
 
     }
 
     public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody);
-    }
-    public static double area (double len){
-      return len * len;
-    }
-    public static double area (double a, double b){
-      return a * b;
 
+        System.out.println("Hello, " + somebody);
     }
 
 
